c70562a5a11afc4587dfdc31472310ab66514f9e
yatin mahajan <yatin428.ym@gmail.com>
2021-04-23 18:17:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,18 @@
 session_start();
 ?>
 
+<?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["us-cdbr-east-03.cleardb.com"];
+$cleardb_username = $cleardb_url["b594c467795321"];
+$cleardb_password = $cleardb_url["4c14ee23"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
 <html>
 
   <head>
